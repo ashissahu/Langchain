e15dc151765c541920ee2a428fb405a05b575388
Ashis Sahu <ashisml302@gmail.com>
2026-06-04 00:48:08 +0530

    LLM Basics

---
+++ LLM_landscape.py
@@ -0,0 +1,16 @@
+# LLM Application Landscape:
+############################
+#1. Simple LLM Calls:
+#   - Use LLMs for basic tasks like text generation, summarization, translation, and question-answering.
+#   - wemadeastory.com
+
+#2. VectorStores(RAG):
+#   - Combine LLMs with vector databases to enable retrieval-augmented generation (RAG).
+#   - quivr.com
+
+#3. Agentic LLMs:
+#   - Develop agents that can perform complex tasks by interacting with LLMs and other tools.
+
+# Agents + VectorStores:
+#   - Integrate agents with vector databases to enhance their capabilities in retrieving and utilizing information.
+#   - autogpt,gpt-engineer,babyagi
